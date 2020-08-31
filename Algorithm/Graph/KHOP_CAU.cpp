507e4253b2eaaceed016daa80f2066a3c0d5906f
Pham Van Trong <55803155+phamtrong2001@users.noreply.github.com>
2020-08-31 22:37:50 +0700

    Update KHOP_CAU.cpp

--- Algorithm/Graph/KHOP_CAU.cpp
+++ Algorithm/Graph/KHOP_CAU.cpp
@@ -13,34 +13,38 @@
 using namespace std;
 
 vector <int> a[maxn];
+
 vector <pii> vcau;
+
 int cau = 0;
+
 bool khop[maxn];
-int n, m, indexx[maxn], low[maxn], Time=0;
+
+int n, m, index[maxn], low[maxn], Time=0;
 
 void visit(int u, int p)
 {
     int snc = 0;
-    low[u] = indexx[u] = ++Time;
+    low[u] = index[u] = ++Time;
     for (int i = 0; i < a[u].size(); i++)
     {
         int v = a[u][i];
         if (v != p)
         {
-            if (indexx[v] != 0) low[u] = min(low[u], indexx[u]);
+            if (index[v] != 0) low[u] = min(low[u], index[v]);
             else
             {
                 visit(v, u);
                 snc++;
                 low[u] = min(low[u], low[v]);
-                if (low[v] >= indexx[v]) cau++; vcau.PB(mp(u, v));
+                if (low[v] >= index[v]) cau++, vcau.PB(mp(u, v));
                 if (u == p)
                 {
                     if (snc >= 2) khop[u] = 1;
                 }
                 else
                 {
-                    if (low[v] >= indexx[u]) khop[u] = 1;
+                    if (low[v] >= index[u]) khop[u] = 1;
                 }
             }
         }
@@ -50,14 +54,16 @@ void visit(int u, int p)
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    //freopen(".inp", "r", stdin);
     cin >> n >> m;
-    for (int i=1; i<=m; i++)
+    for (int i = 1; i <= m; i++)
     {
-        int u, v; cin >> u >> v;
+        int u, v;
+        cin >> u >> v;
         a[u].PB(v);
         a[v].PB(u);
     }
-    for (int i = 1; i <= n; i++) if (!indexx[i]) visit(i, i);
+    for (int i = 1; i <= n; i++) if (!index[i]) visit(i, i);
     int dem = 0;
     for (int i = 1; i <= n; i++) if (khop[i]) dem++;
     cout << dem <<" "<< cau;
